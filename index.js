159be626114f9c9b1ed2a261f451cc4368c5469b
jrc03c <jrc03c@pm.me>
2021-05-07 10:18:39 -0500

    ...

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ Object.defineProperty(Array.prototype, "last", {
   },
 })
 
+Array.prototype.remove = function(item){
+  return this.splice(this.indexOf(item), 1)
+}
+
+function rotatePointAroundZero(point, angle){
+  let radius = getMagnitude(point)
+  let theta = Math.atan(point[1] / point[0])
+  if (point[0] < 0) theta += Math.PI
+
+  return [
+    radius * Math.cos(theta + angle),
+    radius * Math.sin(theta + angle),
+  ]
+}
+
 function dot(a, b){
   return a[0] * b[0] + a[1] * b[1]
 }
@@ -26,8 +41,17 @@ function getMagnitude(v){
   return Math.sqrt(dot(v, v))
 }
 
-function getAngleBetweenVectors(a, b){
-  return Math.acos(dot(a, b) / (getMagnitude(a) * getMagnitude(b)))
+function isRightTurn(p1, p2, p3){
+  // translate all points such that p2 is at the origin
+  let p1Temp = [p1[0] - p2[0], p1[1] - p2[1]]
+  let p2Temp = [0, 0]
+  let p3Temp = [p3[0] - p2[0], p3[1] - p2[1]]
+
+  // rotate all points such that p1Temp lies on the negative y-axis
+  let angle = Math.atan(p1Temp[1] / p1Temp[0])
+  if (p1Temp[0] < 0) angle += Math.PI
+  p3Temp = rotatePointAroundZero(p3Temp, -angle + Math.PI / 2)
+  return p3Temp[0] > 0
 }
 
 function getJarvisConvexHull(points){
@@ -35,18 +59,21 @@ function getJarvisConvexHull(points){
     throw new Error("A minimum of 3 points are needed to compute a hull!")
   }
 
-  let frontier = points.slice().sort((a, b) => a.x - b.x)
-  let hull = [frontier.shift()]
+  let frontier = points.slice().sort((a, b) => a[0] < b[0] ? -1 : 1)
+  let hull = [frontier.first]
 
-  while (hull.length < 3 && hull[0] !== hull[hull.length - 1]){
-    let p1 = hull[hull.length - 1]
-    let p2 = frontier[0]
+  while (hull.length < 3 || hull.last !== hull.first){
+    let p1 = hull.last
+    let p2 = frontier.first === p1 ? frontier[1] : frontier.first
 
     frontier.forEach(p3 => {
-      if (p2 !== p3){
-        let angle = getAngleBetweenVectors()
+      if (p1 !== p3 && p2 !== p3 && isRightTurn(p1, p2, p3)){
+        p2 = p3
       }
     })
+
+    hull.push(p2)
+    frontier.remove(p2)
   }
 
   return hull
